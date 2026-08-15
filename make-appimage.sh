a429268efcc9afd22c6e8aa534305fe4d5e8df72
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:40:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q asunder | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q asunder | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -22,8 +22,6 @@ quick-sharun /usr/bin/asunder \
   /usr/bin/opusenc \
   /usr/bin/wavpack \
   /usr/lib/libcdda*.so*
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
